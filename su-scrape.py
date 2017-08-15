c3f7147affb449b7aa903126b2e7bf2286c7c389
Scott Edenbaum <scott@SME-MBP.home>
2017-08-15 17:46:08 -0400

    scrapes most recent State Union addresses missing from NLTK corpus

---
+++ su-scrape.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+from bs4 import BeautifulSoup
+import requests
+
+
+url = {'2007-GWBush':'http://www.presidency.ucsb.edu/ws/index.php?pid=24446',
+       '2008-Obama':'http://www.presidency.ucsb.edu/ws/index.php?pid=76301',
+       '2009-Obama':'http://www.presidency.ucsb.edu/ws/index.php?pid=85753',
+       '2010-Obama':'http://www.presidency.ucsb.edu/ws/index.php?pid=87433',
+       '2011-Obama':'http://www.presidency.ucsb.edu/ws/index.php?pid=88928',
+       '2012-Obama':'http://www.presidency.ucsb.edu/ws/index.php?pid=99000',
+       '2013-Obama':'http://www.presidency.ucsb.edu/ws/index.php?pid=102826',
+       '2014-Obama':'http://www.presidency.ucsb.edu/ws/index.php?pid=104596',
+       '2015-Obama':'http://www.presidency.ucsb.edu/ws/index.php?pid=108031',
+       '2016-Obama':'http://www.presidency.ucsb.edu/ws/index.php?pid=111174',
+       '2017-Trump':'http://www.presidency.ucsb.edu/ws/index.php?pid=123408'
+       }
+
+for year,link in url.items():
+    print('Scraping State of Union address for %s:' %(year[:4]))
+    out = requests.get(link)   
+    soup = BeautifulSoup(out.content,'lxml') 
+    su_content = soup.findAll('div',{'class':'tools'})[0].next_sibling.text
+    file_f = year + '.txt'
+    f = open(file_f,'w')
+    f.write(su_content)
+    f.close()
